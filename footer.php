7a2c1e41fdeaee4690d32819fcb5fdb70c3e02c2
Pierre Rudloff <contact@rudloff.pro>
2014-03-27 16:05:52 +0100

    Link to GitHub repo

--- footer.php
+++ footer.php
@@ -1,11 +1,12 @@
 <footer>
     <div class="footer_wrapper">
-     &copy; AllTube Download <span itemprop="copyrightYear">2013</span> &middot;
      Code by <a rel="author" target="blank" itemprop="author" href="http://rudloff.pro/">Pierre Rudloff</a>
      &middot; Design by
      <a rel="author" itemprop="author" target="blank" href="http://olivierhaquette.fr">Olivier Haquette</a>
      &middot;
      <a target="_blank" href="https://www.facebook.com/pages/AllTube-Download/571380966249415" title="AllTube Download on Facebook">Like us on Facebook</a>
+     &middot;
+     <a href="https://github.com/Rudloff/alltube">Get the code</a>
      &middot;
      Based on <a href="http://rg3.github.io/youtube-dl/">youtube-dl</a>
      </div>
