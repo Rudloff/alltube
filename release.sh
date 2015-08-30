2b127b05f26325d6deb8a48c4f12c18ec993a366
Pierre Rudloff <contact@rudloff.pro>
2015-08-30 16:17:21 +0200

    Don't include cache in release

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 rm alltube-release.zip
-zip -r alltube-release.zip *.php dist/ fonts/ .htaccess img/ js/ LICENSE README.md robots.txt sitemap.xml  templates/ templates_c/ vendor/ youtube-dl -x config.php
+zip -r alltube-release.zip *.php dist/ fonts/ .htaccess img/ js/ LICENSE README.md robots.txt sitemap.xml  templates/ templates_c/ vendor/ youtube-dl -x config.php templates_c/*
