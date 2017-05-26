e8d989a9f26d8fc4e4dfe317e131c4ab4c8e33ab
Unknown <tony19955569@gmail.com>
2017-05-26 15:04:33 -0700

    Fix syntax by styleci

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 
-$language = "zh_CN";
-putenv("LANG=".$language);
+$language = 'zh_CN';
+putenv('LANG='.$language);
 setlocale(LC_ALL, [$language, $language.'.utf8']);
 
 require_once __DIR__.'/vendor/autoload.php';
