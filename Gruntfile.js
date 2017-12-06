d3ea76faaf088cc145495e8a0acb16c7abcd490f
Pierre Rudloff <contact@rudloff.pro>
2017-12-06 13:31:25 +0100

    We don't need the xdebug workaround anymore (see https://getcomposer.org/doc/articles/troubleshooting.md#xdebug-impact-on-composer)

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,7 @@ module.exports = function (grunt) {
             },
             phpunit: {
                 options: {
-                    bin: 'php -dzend_extension=xdebug.so ./vendor/bin/phpunit',
+                    bin: 'vendor/bin/phpunit',
                     stopOnError: true,
                     stopOnFailure: true,
                     followOutput: true
